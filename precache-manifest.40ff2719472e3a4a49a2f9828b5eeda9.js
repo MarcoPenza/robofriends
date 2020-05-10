0beefd73ba25b55dfdb0b82afe1c6b0a97a6fd14
MarcoPenza <62351711+MarcoPenza@users.noreply.github.com>
2020-05-10 15:50:04 +0200

    Updates

---
+++ precache-manifest.40ff2719472e3a4a49a2f9828b5eeda9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081aef69e0077b446f9f64506095a9bc",
+    "url": "/MarcoPenza/robofriends.git/index.html"
+  },
+  {
+    "revision": "825a8413395861e7b9f2",
+    "url": "/MarcoPenza/robofriends.git/static/css/main.3bb8d4b2.chunk.css"
+  },
+  {
+    "revision": "f8a2aea2823b2a2ee05d",
+    "url": "/MarcoPenza/robofriends.git/static/js/2.3d5a3330.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MarcoPenza/robofriends.git/static/js/2.3d5a3330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "825a8413395861e7b9f2",
+    "url": "/MarcoPenza/robofriends.git/static/js/main.ac715007.chunk.js"
+  },
+  {
+    "revision": "81c90e0ac11056c877a6",
+    "url": "/MarcoPenza/robofriends.git/static/js/runtime-main.179c02e9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MarcoPenza/robofriends/precache-manifest.c3778381628acefaada7d6c129e006ce.js"
+  "/MarcoPenza/robofriends.git/precache-manifest.40ff2719472e3a4a49a2f9828b5eeda9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MarcoPenza/robofriends/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MarcoPenza/robofriends.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
