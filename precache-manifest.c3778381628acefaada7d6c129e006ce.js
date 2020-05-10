85d6ddc6d5303cead3ba5d54af1c25247940eb25
MarcoPenza <62351711+MarcoPenza@users.noreply.github.com>
2020-05-10 15:39:10 +0200

    Updates

---
+++ precache-manifest.c3778381628acefaada7d6c129e006ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c4c6f122ec611f94507ed1b69812352",
+    "url": "/MarcoPenza/robofriends/index.html"
+  },
+  {
+    "revision": "34e7ea5fc47967308d0d",
+    "url": "/MarcoPenza/robofriends/static/css/main.3bb8d4b2.chunk.css"
+  },
+  {
+    "revision": "f8a2aea2823b2a2ee05d",
+    "url": "/MarcoPenza/robofriends/static/js/2.3d5a3330.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MarcoPenza/robofriends/static/js/2.3d5a3330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34e7ea5fc47967308d0d",
+    "url": "/MarcoPenza/robofriends/static/js/main.fd85b865.chunk.js"
+  },
+  {
+    "revision": "08443db934e4346c438a",
+    "url": "/MarcoPenza/robofriends/static/js/runtime-main.0ddb2e43.js"
+  }
+]);
